adf22f67c7106173ecea7bf316ae512794944bc6
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-11-21 22:30:23 -0500

    decrease sleep time, gevent monkey patch
    This finally fixes the issues with production by allowing asynchronous
    requests.

--- ctfengine/sse.py
+++ ctfengine/sse.py
@@ -1,7 +1,9 @@
 import gevent
+import gevent.monkey
 import redis
 from ctfengine import config
 
+gevent.monkey.patch_all()
 red = redis.StrictRedis()
 
 
@@ -9,7 +11,7 @@ def event_stream():
     pubsub = red.pubsub()
     pubsub.subscribe(config.REDIS_CHANNEL)
     for msg in pubsub.listen():
-        gevent.sleep(2)
+        gevent.sleep(0.1)
         yield 'data: {0}\n\n'.format(msg['data'])
 
 
