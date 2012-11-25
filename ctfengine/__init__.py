4fb17d54609790d31af3da32e1fcec462ef75693
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-11-24 20:10:17 -0500

    email admin about errors

--- ctfengine/__init__.py
+++ ctfengine/__init__.py
@@ -7,3 +7,23 @@ app.config.from_object(config)
 app.request_class = lib.Request
 
 import ctfengine.views
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler('127.0.0.1',
+                               app.config['MAIL_FROM'],
+                               app.config['ADMINS'], "ctfengine error")
+    mail_handler.setFormatter(logging.Formatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+
+Message:
+
+%(message)s
+'''))
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
